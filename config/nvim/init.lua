35de861b4f29a26457ead69659c822ec44524283
vidy007 <apoteraev65@gmail.com>
2026-04-05 12:50:20 +0700

    nvim config cleaning

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,5 @@
+-- LAZY.NVIM SETUP --
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -14,6 +16,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
+
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
@@ -23,151 +26,75 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-   { "neovim/nvim-lspconfig",
-   lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-  dependencies = {
-    -- main one
-    { "ms-jpq/coq_nvim", branch = "coq" },
+    {
+	  "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+	  lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+	  dependencies = {
+	    -- main one
+	    { "ms-jpq/coq_nvim", branch = "coq" },
 
-    -- 9000+ Snippets
-    { "ms-jpq/coq.artifacts", branch = "artifacts" },
+	    -- 9000+ Snippets
+	    { "ms-jpq/coq.artifacts", branch = "artifacts" },
 
-    -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-    -- Need to **configure separately**
-    { 'ms-jpq/coq.thirdparty', branch = "3p" }
-    -- - shell repl
-    -- - nvim lua api
-    -- - scientific calculator
-    -- - comment banner
-    -- - etc
+	    -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+	    -- Need to **configure separately**
+	    { 'ms-jpq/coq.thirdparty', branch = "3p" }
+	    -- - shell repl
+	    -- - nvim lua api
+	    -- - scientific calculator
+	    -- - comment banner
+	    -- - etc
+	  },
+	  init = function()
+	    vim.g.coq_settings = {
+		auto_start = true, -- if you want to start COQ at startup
+		-- Your COQ settings here
+	    }
+	  end,
+	  config = function()
+	    -- Your LSP settings here
+	  end,
+    },
+    { "nvim-tree/nvim-web-devicons"},
+    { "nvim-lua/lsp-status.nvim" },
+    { "williamboman/mason.nvim" },
+    { "nvim-lua/plenary.nvim" },
+    -- { "BurntSushi/ripgrep" },
+    { "sharkdp/fd" }, 
+    { "brenoprata10/nvim-highlight-colors" },
+    { "neovim/nvim-lspconfig" },
+    { "rose-pine/neovim" },
+    { "AckslD/swenv.nvim" },
+    { "roobert/f-string-toggle.nvim" },
+    { "neolooong/whichpy.nvim" },
+    { "smzm/hydrovim" },
+    { "bfredl/nvim-ipy" },
+    { "echasnovski/mini.nvim" },
+    { "deathbeam/autocomplete.nvim" },
+    {
+	    'windwp/nvim-autopairs',
+	    event = "InsertEnter",
+	    config = false
+	    -- use opts = {} for passing setup options
+	    -- this is equivalent to setup({}) function
+    },
   },
-  init = function()
-    vim.g.coq_settings = {
-        auto_start = true, -- if you want to start COQ at startup
-        -- Your COQ settings her
-    }
-  end,
-  config = function()
-    -- Your LSP settings here
-  end,
-},
-   { "nvim-tree/nvim-web-devicons"},
-   -- { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-   { "nvim-lua/lsp-status.nvim" },
-   { "williamboman/mason.nvim" },
-   { "AckslD/swenv.nvim" },
-   { "roobert/f-string-toggle.nvim" },
-   { "neolooong/whichpy.nvim" },
-   { "smzm/hydrovim" },
-   { "bfredl/nvim-ipy" },
-   { "echasnovski/mini.nvim" },
-   { "deathbeam/autocomplete.nvim" },
-   { "nvim-lua/plenary.nvim" },
-   { "BurntSushi/ripgrep" },
-   { "sharkdp/fd" }, 
-   { "brenoprata10/nvim-highlight-colors" },
-   { 
-	   "windwp/nvim-autopairs",
-	   event = "InsertEnter",
-	   config = true
-   },
-   { "mfussenegger/nvim-dap" },
-   { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
-   { "folke/neodev.nvim" },
-   { "rose-pine/neovim" },
-   {
-	'nvim-telescope/telescope.nvim',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		-- optional but recommended
-		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-	}
-   },
-},
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "gruvbox" } },
+  install = { colorscheme = { "rose-pine" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
--- require('lualine').setup {
--- 	require('lualine').setup {
---   options = {
---     icons_enabled = true,
---     theme = 'gruvbox',
---     component_separators = { left = '', right = ''},
---     section_separators = { left = '', right = ''},
---     disabled_filetypes = {
---       statusline = {},
---       winbar = {},
---     },
---     ignore_focus = {},
---     always_divide_middle = true,
---     globalstatus = false,
---     refresh = {
---       statusline = 1000,
---       tabline = 1000,
---       winbar = 1000,
---     }
---   },
---   sections = {
---     lualine_a = {'mode'},
---     lualine_b = {},
---     lualine_c = {'filename'},
---     lualine_x = {'encoding', 'filetype'},
---     lualine_y = {'progress'},
---     lualine_z = {'location'}
---   },
---   inactive_sections = {
---     lualine_a = {},
---     lualine_b = {},
---     lualine_c = {'filename'},
---     lualine_x = {'location'},
---     lualine_y = {},
---     lualine_z = {}
---   },
---   tabline = {
---   	lualine_a = {'buffers'},
---   	lualine_b = {'branch'},
---   	lualine_c = {'diff'},
---   	lualine_x = {},
---   	lualine_y = {},
---   	lualine_z = {'tabs'}
---   },
---   winbar = {},
---   inactive_winbar = {},
---   extensions = {}
--- }
--- }
-require("neodev").setup({
-  library = { plugins = { "nvim-dap-ui" }, types = true }, 
-})
-require("dapui").setup()
-local dap = require('dap')
-dap.configurations.python = {
-      {
-        type = 'gdb';
-        request = 'launch';
-        name = "Launch file";
-        program = "${file}";
-        pythonPath = function()
-          return '/usr/bin/python'
-        end;
-      },
-}
+-- END OF LAZY.NVIM SETUP --
+
+-- PLUGINS SETUP --
 require('mini.pick').setup()
 require('mini.icons').setup()
 require('mini.starter').setup()
 require('mini.statusline').setup()
 require('mini.tabline').setup()
 require('mini.surround').setup()
---require("autoclose").setup({
---	options = {
---		disable_when_touch = {"false"},
---		auto_indent = {"true"},
---	},
---})
 vim.lsp.enable('python')
 require('nvim-highlight-colors').setup({})
 require('mini.clue').setup()
@@ -199,46 +126,6 @@ vim.lsp.config('pylsp', {
     }
   }
 })
-require("dap").adapters.gdb = {
-  type = "executable",
-  command = "gdb",
-  args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
-}
-require("dap").configurations.rust = {
-  {
-    name = "Launch",
-    type = "gdb",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = "${workspaceFolder}",
-    stopAtBeginningOfMainSubprogram = false,
-  },
-  {
-    name = "Select and attach to process",
-    type = "gdb",
-    request = "attach",
-    program = function()
-       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    pid = function()
-       local name = vim.fn.input('Executable name (filter): ')
-       return require("dap.utils").pick_process({ filter = name })
-    end,
-    cwd = '${workspaceFolder}'
-  },
-  {
-    name = 'Attach to gdbserver :1234',
-    type = 'gdb',
-    request = 'attach',
-    target = 'localhost:1234',
-    program = function()
-       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}'
-  },
-}
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme rose-pine]])
 vim.wo.number = true
@@ -297,3 +184,6 @@ miniclue.setup({
     miniclue.gen_clues.z(),
   },
 })
+-- END OF PLUGINS SETUP --
+
+
